9425da2709a5bd7ac810dea3480fa32f4186e801
ibra077 <amar.ibrahimovic@student.um.si>
2024-05-11 12:43:08 +0200

    [WOMP-22] Connected to MongoDB

--- backend/main.js
+++ backend/main.js
@@ -1,5 +1,6 @@
 // Libraries
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 // Routes
@@ -10,6 +11,18 @@ const port = process.env.DEV_PORT;
 // Basic setup
 const app = express();
 
+// // Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING, { dbName: "website" })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB:", error);
+  });
+
+
+// // Base routes
 app.get("/", (req, res) => {
   res.send("You can access the API");
 });
