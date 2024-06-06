3a152b1e0a89a0246133dfe7206b06717374ffe8
VesligajMisel <105655188+VesligajMisel@users.noreply.github.com>
2024-06-06 09:23:24 +0200

    Add files via upload
    Dodana primera uporabe service-ov
    
    WOMP-46

---
+++ TraficServiceExample.kt
@@ -0,0 +1,17 @@
+package org.example
+
+import org.example.service.TrafficService
+
+/*fun main() {
+    val service = TrafficService()
+    val trafficInfos = service.fetchTrafficInfo()
+    println("Number of traffic infos: ${trafficInfos.size}")
+    trafficInfos.forEach { info ->
+        println("Traffic ID: ${info.id}")
+        println("Description: ${info.description}")
+        println("Location: ${info.location}")
+        println("Date: ${info.date}")
+        println("Type: ${info.type}")
+        println()
+    }
+}*/
