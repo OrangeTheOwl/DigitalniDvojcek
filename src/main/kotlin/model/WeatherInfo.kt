0bdc7384923863b10e74e1789179363e9d62e971
Mišel Vešligaj <miselvesligajj@gmail.com>
2024-06-09 15:31:29 +0200

    Dodan WeatherInfo model
    WOMP-87

---
+++ src/main/kotlin/model/WeatherInfo.kt
@@ -0,0 +1,23 @@
+package model
+
+data class WeatherInfo (
+    val id: String,
+    val location: String,
+    val temperature: Int,
+    val humidity: Int,
+    val humidityStatus : String,
+    val windSpeed: Int,
+    val windStatus : String,
+    val status: String
+){
+    override fun toString(): String {
+        return "ID: $id\n" +
+                "Location: $location\n" +
+                "Temperature: $temperature\n" +
+                "Humidity: $humidity\n" +
+                "Humidity Status: $humidityStatus\n" +
+                "Wind Speed: $windSpeed\n" +
+                "Wind Status: $windStatus\n" +
+                "Status: $status"
+    }
+}
