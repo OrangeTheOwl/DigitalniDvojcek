61ec75195baa0642462a71584bfa7fbe50cf3ca2
ibra077 <amar.ibrahimovic@student.um.si>
2024-06-09 14:02:31 +0200

    [WOMP-65]

---
+++ frontend/src/pages/TrafficInfo.jsx
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from "react";
+import {
+  Card,
+  CardContent,
+  Container,
+  Typography,
+  TextField,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import Header from "../components/Header";
+
+const TrafficInfo = () => {
+  const [username, setUsername] = useState("");
+  const [userId, setUserId] = useState("");
+  const [locationDisplay, setLocationDisplay] = useState("");
+  const [traficInfos, setTrafficInfos] = useState([]);
+  const [selectedStatus, setSelectedStatus] = useState("");
+
+  const handleChange = (event) => {
+    setSelectedStatus(event.target.value);
+  };
+
+  const fetchTrafficInfos = async (locationId) => {
+    const response = await fetch(
+      `http://localhost:3210/trafficInfo/location/${locationId}`
+    );
+    const data = await response.json();
+    setTrafficInfos(data);
+  };
+
+  const checkAuth = async () => {
+    try {
+      const res = await fetch("http://localhost:3210/user/check", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        console.log(data);
+        setUserId(data._id);
+        setUsername(data.username);
+        setLocationDisplay(
+          `${data.defaultLocation.city}, ${data.defaultLocation.address}`
+        );
+        await fetchTrafficInfos(data.defaultLocation._id);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    checkAuth();
+    // fetchTrafficInfos();
+  }, []);
+
+  return (
+    <Container>
+      <Header page="Traffic Info" />
+      <TextField
+        disabled
+        fullWidth
+        id="outlined-basic"
+        label={locationDisplay}
+        variant="outlined"
+        sx={{ my: 2 }}
+      />
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">
+          Filtriraj po statusu
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={selectedStatus}
+          label="Filtriraj po statusu"
+          onChange={handleChange}
+        >
+          <MenuItem value="">Vsi statusi</MenuItem>
+          <MenuItem value="brez zastojev">Brez zastojev</MenuItem>
+          <MenuItem value="zastoj">Zastoj</MenuItem>
+          <MenuItem value="nesreča">Nesreča</MenuItem>
+          <MenuItem value="delo na cesti">Delo na cesti</MenuItem>
+        </Select>
+      </FormControl>
+      <div>
+        {traficInfos
+          .filter((info) => !selectedStatus || info.status === selectedStatus)
+          .map((trafficInfo) => (
+            <Card key={trafficInfo.id} sx={{ my: 2 }}>
+              <CardContent>
+                <Typography variant="h4">
+                  Lokacija: {trafficInfo.location.city},{" "}
+                  {trafficInfo.location.address}
+                </Typography>
+                <Typography variant="h6">
+                  Čas:{" "}
+                  {new Date(
+                    new Date(trafficInfo.time).toISOString().slice(0, -1)
+                  ).toLocaleString("de-DE")}
+                </Typography>
+                <Typography variant="h6">
+                  Status: {trafficInfo.status}
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+      </div>
+    </Container>
+  );
+};
+
+export default TrafficInfo;
---
+++ frontend/src/pages/WeatherConditions.jsx
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from "react";
+import {
+  Card,
+  CardContent,
+  Container,
+  Typography,
+  TextField,
+} from "@mui/material";
+import Header from "../components/Header";
+
+const WeatherConditions = () => {
+  const [username, setUsername] = useState("");
+  const [userId, setUserId] = useState("");
+  const [locationDisplay, setLocationDisplay] = useState("");
+  const [weatherConditions, setWeatherConditions] = useState([]);
+
+  const fetchWeatherConditions = async (locationId) => {
+    const response = await fetch(
+      `http://localhost:3210/weatherCondition/location/${locationId}`
+    );
+    const data = await response.json();
+    console.log(data);
+    setWeatherConditions(data);
+  };
+
+  const checkAuth = async () => {
+    try {
+      const res = await fetch("http://localhost:3210/user/check", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        setUserId(data._id);
+        setUsername(data.username);
+        setLocationDisplay(
+          `${data.defaultLocation.city}, ${data.defaultLocation.address}`
+        );
+        await fetchWeatherConditions(data.defaultLocation._id);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    checkAuth();
+    fetchWeatherConditions();
+  }, []);
+
+  return (
+    <Container>
+      <Header page="Weather Conditions" />
+      <TextField
+        disabled
+        fullWidth
+        id="outlined-basic"
+        label={locationDisplay}
+        variant="outlined"
+      />
+      <div>
+        {weatherConditions.map((weatherCondition) => (
+          <Card key={weatherCondition.id} sx={{ my: 2 }}>
+            <CardContent>
+              <Typography variant="h4">
+                Lokacija: {weatherCondition.location.city},{" "}
+                {weatherCondition.location.address}
+              </Typography>
+              <Typography variant="h6">
+                Čas:{" "}
+                {new Date(
+                  new Date(weatherCondition.time).toISOString().slice(0, -1)
+                ).toLocaleString("de-DE")}
+              </Typography>
+              <Typography variant="h6">
+                Temperature: {weatherCondition.temperature}
+              </Typography>
+              <Typography variant="h6">
+                Vlaga: {weatherCondition.humidity}
+              </Typography>
+              <Typography variant="h6">
+                Veter: {weatherCondition.wind_speed}
+              </Typography>
+              <Typography variant="h6">
+                Status: {weatherCondition.status}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default WeatherConditions;
