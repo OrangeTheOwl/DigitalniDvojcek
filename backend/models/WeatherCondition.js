381c4ebae2fed054cd9da83a58e050d907504ae0
ibra077 <amar.ibrahimovic@student.um.si>
2024-06-09 14:07:19 +0200

    [WOMP-87] WeatherCondition model

--- backend/models/WeatherCondition.js
+++ backend/models/WeatherCondition.js
@@ -1,34 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const weatherConditionSchema = new mongoose.Schema({
-    time: {
-        type: Date,
-        required: true
-    },
-    location: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Location',
-        required: true
-    },
-    temperature: {
-        type: Number,
-        required: true
-    },
-    humidity: {
-        type: Number,
-        required: true
-    },
-    wind_speed: {
-        type: Number,
-        required: true
-    },
-    status: {
-        type: String,
-        enum: ['sončno', 'oblačno', 'dež', 'megla'],
-        required: true
-    }
+  time: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "location",
+    required: true,
+  },
+  temperature: {
+    type: Number,
+    required: true,
+  },
+  humidity: {
+    type: Number,
+    required: true,
+  },
+  wind_speed: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["sončno", "oblačno", "dež", "megla"],
+    required: true,
+  },
 });
 
-const WeatherCondition = mongoose.model('WeatherCondition', weatherConditionSchema);
+const WeatherCondition = mongoose.model(
+  "WeatherCondition",
+  weatherConditionSchema
+);
 
 module.exports = WeatherCondition;
