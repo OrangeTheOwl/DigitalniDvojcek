99a4d08f215b251911e1f0f4b9ae64629c2527f4
ibra077 <amar.ibrahimovic@student.um.si>
2024-06-10 19:05:32 +0200

    [WOMP-102] Gumb za dodajanje in brisanje priljubljenih letov

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 // Icons
@@ -12,37 +12,117 @@ import PersonIcon from "@mui/icons-material/Person";
 import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
 
 const Header = ({ page }) => {
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        my: 2,
-        alignItems: "center",
-      }}
-    >
-      <Typography variant="h3">{page}</Typography>
-      <Box sx={{ display: "flex", gap: 2 }}>
-        <Link to="/map">
-          <MapIcon />
-        </Link>
-        <Link to="/trafficInfo">
-          <TrafficIcon />
-        </Link>
-        <Link to="/weatherConditions">
-          <WbSunnyIcon />
-        </Link>
-        <Link to="/graph">
-          <BarChartIcon />
-        </Link>
-        <Link to="/profile">
-          <PersonIcon />
-        </Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+  const [username, setUsername] = useState("");
+  const [userId, setUserId] = useState("");
+  const [locationId, setLocationId] = useState("");
+
+  const checkAuth = async () => {
+    try {
+      const res = await fetch("http://localhost:3210/user/check", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        setUserId(data._id);
+        setUsername(data.username);
+        setLocationId(data.defaultLocation._id);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      const res = await fetch("http://localhost:3210/user/logout", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+
+      if (res.ok) {
+        window.location.href = "/";
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useState(() => {
+    checkAuth();
+  }, []);
+
+  if (userId !== "") {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          my: 2,
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h3">{page}</Typography>
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <Link to="/">
+            <MapIcon />
+          </Link>
+          <Link to="/graph">
+            <BarChartIcon />
+          </Link>
+          <Link to="/trafficInfo">
+            <TrafficIcon />
+          </Link>
+          <Link to="/weatherConditions">
+            <WbSunnyIcon />
+          </Link>
+          <Link to="/profile">
+            <PersonIcon />
+          </Link>
+          <Typography>{username}</Typography>
+          <Link to="/" onClick={handleLogout}>
+            Odjava
+          </Link>
+        </Box>
       </Box>
-    </Box>
-  );
+    );
+  } else {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          my: 2,
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h3">{page}</Typography>
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <Link to="/">
+            <MapIcon />
+          </Link>
+          <Link to="/graph">
+            <BarChartIcon />
+          </Link>
+          <Link to="/trafficInfo">
+            <TrafficIcon />
+          </Link>
+          <Link to="/weatherConditions">
+            <WbSunnyIcon />
+          </Link>
+          <Link to="/login">Prijava</Link>
+          <Link to="/register">Registracija</Link>
+        </Box>
+      </Box>
+    );
+  }
 };
 
 export default Header;
