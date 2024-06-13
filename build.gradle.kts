0373e7420a372c64ba0609490a5db80c7a4c43bd
Mišel Vešligaj <miselvesligajj@gmail.com>
2024-06-13 23:20:45 +0200

    Dodani novi dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,14 +15,13 @@ repositories {
 }
 
 dependencies {
-    // Note, if you develop a library, you should use compose.desktop.common.
-    // compose.desktop.currentOs should be used in launcher-sourceSet
-    // (in a separate module for demo project and in testMain).
-    // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
     implementation("it.skrape:skrapeit:1.1.5")
     implementation("org.seleniumhq.selenium:selenium-java:3.141.59")
     implementation("org.seleniumhq.selenium:selenium-chrome-driver:3.141.59")
+    implementation("io.github.serpro69:kotlin-faker:1.12.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation("com.google.code.gson:gson:2.8.6")
 }
 
 compose.desktop {
