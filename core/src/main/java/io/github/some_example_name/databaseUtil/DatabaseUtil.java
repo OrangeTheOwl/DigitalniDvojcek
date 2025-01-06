5b88bd9bc2677b15c9bdd078f9efb1e7471d372c
OrangeTheOwl <90248618+OrangeTheOwl@users.noreply.github.com>
2025-01-07 00:51:41 +0100

    Database Connection

---
+++ core/src/main/java/io/github/some_example_name/databaseUtil/DatabaseUtil.java
@@ -0,0 +1,32 @@
+package io.github.some_example_name.databaseUtil;
+
+import static com.mongodb.client.model.Filters.eq;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+
+public class DatabaseUtil {
+    public static void main( String[] args ) {
+
+        // Replace the placeholder with your MongoDB deployment's connection string
+        String uri = "";
+
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("website");
+            MongoCollection<Document> collection = database.getCollection("airports");
+
+            Document doc = collection.find(eq("name", "Letališče 1")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
+    }
+
+}
+
